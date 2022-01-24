5b09d36093ff9dacfa3480e12cc440ed70cccf88
abhishek <abhishekmazumder69@gmail.com>
2022-01-24 23:34:16 +0530

    Add About & NotFound page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
 	return (
 		<Router>
-			<div className="flex flex-col justify-between h-screen">
+			<div className="flex flex-col justify-between h-screen align-center">
 				<Navbar />
-				<main className="container px-3 pb-10 mx-auto">Content</main>
+				<main className="container mx-auto px-4 pb-10">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/notfound" element={<NotFound />} />
+						<Route path="/*" element={<NotFound />} />
+					</Routes>
+				</main>
 				<Footer />
 			</div>
 		</Router>
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,9 +5,9 @@ import PropTypes from 'prop-types';
 const Navbar = ({ title }) => {
 	return (
 		<nav className="mb-12 shadow-lg navbar bg-neutral text-neutral-content">
-			<div className="container mx-auto">
+			<div className="container px-4 mx-auto">
 				{/* Navbar Logo & Title */}
-				<div className="flex-none px-2 mx-2">
+				<div className="flex-none px-2">
 					<FaGithub className="inline mr-3 text-3xl" />
 					<Link to="/" className="text-lg font-bold align-middle">
 						{title}
